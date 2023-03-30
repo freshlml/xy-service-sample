339d23605773e88329194ab23b3c7089b53b3fb5
freshlml <1326191992@qq.com>
2023-03-30 11:05:15 +0800

    1

--- src/main/java/com/fresh/xy/sample/entity/scan/SampleScan.java
+++ src/main/java/com/fresh/xy/sample/entity/scan/SampleScan.java
@@ -21,8 +21,8 @@ public class SampleScan extends BaseEntity<Long> {
 
     private String name;
     private ScanTypeEnum scanType;
-    //@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss", timezone="GMT+8")
-    //@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss", timezone="GMT+8")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime scanTime;
 
 }
